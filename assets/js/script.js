f927f6ba67dcc776deb05e5dae3ae70520325a64
Abel Yohanes <k9ja1xwk@students.codeinstitute.net>
2024-12-13 18:23:51 +0000

    post request function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
-const API_KEY = "HyWdxV5SfoJNz23cUfj6MbTywEOQ";
+const API_KEY = "HyWdxV5SfoJNz3cUfj6MbTywEOQ";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-async function getStatus(e) {
+async function postForm(e) {
 
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const form = new FormData(document.getElementById("checksform"));
 
-    const response = await fetch(queryString);
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
 
     const data = await response.json();
 
     if (response.ok) {
-        displayStatus(data);
+        console.log(data);
     } else {
         throw new Error(data.error);
     }
