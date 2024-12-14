c21ce9aa06fdeeaf0b3b863876a7944000ccf505
Abel Yohanes <k9ja1xwk@students.codeinstitute.net>
2024-12-14 10:20:12 +0000

    get post added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+
+
 async function postForm(e) {
 
     const form = new FormData(document.getElementById("checksform"));
@@ -20,11 +22,38 @@ async function postForm(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayErrors(data);
     } else {
         throw new Error(data.error);
     }
+}
 
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+async function displayErrors(data) {
+    let heading = `JS Hint results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span`
+        for (let error of data.error_list) {
+            results += `<div>At line<span class="line">${error.line}</span`
+            results += `Column<span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
 }
 
 function displayStatus(data) {
